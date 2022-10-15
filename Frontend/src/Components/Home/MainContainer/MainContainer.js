bc6de44d4a371fe46c13b600a77584156dd76e99
rahul.suvaidyam <rahul.suvaidyam@gmail.com>
2022-10-15 16:47:58 +0530

    add card

--- Frontend/src/Components/Home/MainContainer/MainContainer.js
+++ Frontend/src/Components/Home/MainContainer/MainContainer.js
@@ -1,28 +1,65 @@
 import React from 'react'
 import { BsHeart } from "react-icons/bs";
+import { Datas } from '../../data';
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
 const MainContainer = () => {
+    const responsive = {
+        desktop: {
+            breakpoint: { max: 3000, min: 1024 },
+            items: 1,
+            slidesToSlide: 1 // optional, default to 1.
+        },
+        tablet: {
+            breakpoint: { max: 1024, min: 464 },
+            items: 1,
+            slidesToSlide: 1 // optional, default to 1.
+        },
+        mobile: {
+            breakpoint: { max: 464, min: 100 },
+            items: 1,
+            slidesToSlide: 1 // optional, default to 1.
+        }
+    };
     return (
         <>
-            <div className='px-24 pt-8 p-5 bg-[#f6f9fc]'>
-                <div className='flex flex-col gap-3'>
-                    <h1 className='text-2xl font-bold'>Flash Delas</h1>
-                    <div className="overflow-x-scroll">
+            <Carousel responsive={responsive}
+                swipeable={false}
+                draggable={false}
+                infinite={true}
+                autoPlay={false}
+                autoPlaySpeed={2000}
+                keyBoardControl={true}
+                transitionDuration={500}
+                containerClass="carousel-container"
+                removeArrowOnDeviceType={["tablet", "mobile"]}
+
+            >
+                <div className='px-24 pt-8 p-5 bg-[#f6f9fc]'>
+                    <div className='flex flex-col gap-3'>
+                        <h1 className='text-2xl font-bold'>Flash Delas</h1>
                         <div className="flex w-full gap-6">
-                            <div className="w-1/4 shadow-md p-3 rounded-md bg-white group">
-                                <div className="flex justify-between">
-                                    <div className="px-2 py-1 rounded-xl bg-yellow-400">
-                                        <p className='text-xs font-bold text-white'>50% off</p>
+                            {Datas && Datas.map(item => (
+                                <div key={item.id} className="w-1/4 flex flex-col justify-between  shadow-md p-3 rounded-md bg-white group">
+                                    <div className="flex justify-between">
+                                        <div className="px-2 py-1 rounded-xl bg-yellow-400">
+                                            <p className='text-xs font-bold text-white'>50% off</p>
+                                        </div>
+                                        <div className="hidden group-hover:block">
+                                            <BsHeart className='cursor-pointer text-xl' />
+                                        </div>
                                     </div>
-                                    <div className="hidden group-hover:block">
-                                        <BsHeart />
+                                    <div className="flex flex-col justify-center items-center mt-8">
+                                        <img className='' src={item.img} alt="" />
                                     </div>
+                                    <h2 className='font-medium'>{item.name}</h2>
                                 </div>
-                            </div>
+                            ))}
                         </div>
                     </div>
                 </div>
-            </div>
+            </Carousel>
         </>
     )
 }
---
+++ Frontend/src/Components/data.js
@@ -0,0 +1,27 @@
+
+export const Datas = [
+    {
+        id: 1,
+        img: 'https://rukminim1.flixcart.com/image/200/200/xif0q/smartwatch/k/a/b/-original-imagh94fggh3chyj.jpeg?q=70',
+        name: 'BoAt Watch',
+        price: 599
+    },
+    {
+        id: 2,
+        img: 'https://rukminim1.flixcart.com/image/200/200/kyhlfgw0/smartwatch/9/7/j/-original-imagaptbtq6ukjms.jpeg?q=70',
+        name: 'Watch',
+        price: 1599
+    },
+    {
+        id: 3,
+        img: 'https://rukminim1.flixcart.com/image/200/200/camera/c/h/h/sony-cyber-shot-dsc-h300-advance-point-and-shoot-original-imadpzjmc3hwsnf7.jpeg?q=70',
+        name: 'Black Camera',
+        price: 499
+    },
+    {
+        id: 4,
+        img: 'https://rukminim1.flixcart.com/image/200/200/l1v1uvk0/tablet/9/c/s/6650034-realme-original-imagdc5ac3y7xgxh.jpeg?q=70',
+        name: 'Tablets',
+        price: 5099
+    },
+]
